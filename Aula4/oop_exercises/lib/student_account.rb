781ef5bb35df598b2af9c1f371d821833da2886e
joaobmsf <joao.batista665@gmail.com>
2014-04-01 21:02:14 -0300

    Adicionado as especificacoes para StudentAccount: monthly_fee e withdraw.

--- Aula4/oop_exercises/lib/student_account.rb
+++ Aula4/oop_exercises/lib/student_account.rb
@@ -1,4 +1,17 @@
 require_relative 'checking_account'
 
 class StudentAccount < CheckingAccount
+	@@discount = 1.0
+
+	def initialize
+		super
+		@monthly_fee = MONTHLY_FEE*@@discount - MONTHLY_FEE*@@discount
+	end
+	
+	def withdraw(amount)
+	if @balance - amount >= 0
+		@balance -= amount
+		log_transaction('Withdrawal', amount)
+	end
+  end
 end
